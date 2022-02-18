2770980561d64f1ac020ae03cd3ada30c4ae61b9
Pycomet <alfredemmanuelinyang@gmail.com>
2022-02-18 03:57:03 +0100

    update bug

--- main/vendor.py
+++ main/vendor.py
@@ -24,7 +24,7 @@ def vendor(msg):
             f"Welcome to your Vendor Panel {msg.from_user.first_name}",
             reply_markup=vendor_menu(msg)
         )
-    elif msg.from_user.id == ADMIN_ID:
+    elif int(msg.from_user.id) == int(ADMIN_ID):
 
         bot.reply_to(
             msg,
@@ -34,7 +34,7 @@ def vendor(msg):
     else:
         bot.reply_to(
             msg,
-            f"{vendor} {ADMIN_ID} You are not a vendor, contact support!"
+            f"{vendor} {ADMIN_ID} You are gnot a vendor, contact support!"
         )
 
 
